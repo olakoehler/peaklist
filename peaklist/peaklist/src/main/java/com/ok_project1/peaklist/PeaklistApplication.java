f29defe77ea8b6878b8e308e13305573bfbfa5f8
Ola <ola.koehler@gmail.com>
2026-05-08 10:40:57 +0200

    Add Spring Boot project skeleton with Maven dependencies

---
+++ peaklist/peaklist/src/main/java/com/ok_project1/peaklist/PeaklistApplication.java
@@ -0,0 +1,13 @@
+package com.ok_project1.peaklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeaklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeaklistApplication.class, args);
+	}
+
+}
---
+++ peaklist/peaklist/src/test/java/com/ok_project1/peaklist/PeaklistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ok_project1.peaklist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeaklistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
